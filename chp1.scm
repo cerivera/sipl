dcf6dab035627e3d1530a24cababb1485f5ee027
Carlos Rivera <carlosrivera@Carloss-Mac-mini.local>
2020-03-18 06:52:16 -0500

    3-18-2020
    Next is Exercise 1.7

--- chp1.scm
+++ chp1.scm
@@ -1,4 +1,4 @@
-; Exercise 1.3
+; -- Exercise 1.3
 
 (define (square x) (* x x))
 
@@ -13,4 +13,22 @@
 (= (two-biggest 10 10 20) (sum-of-squares 10 20))
 (= (two-biggest 10 30 30) (sum-of-squares 30 30))
 
+
+; A combination is a combination of operator and operands.
+; (+ 1 2)
+
+
+
+; Applicative-order evaluation: Evaluate the arguments and then apply the operator is what the interpreter uses.  it helps avoid calculating the same expression more than once if we were to expand an expression by continuing to substitute operands as parameters
+
+; Normal-order evaluation: only evaluate the operands when you need to.  Subsitutde operands as parameters until you arrive at an expression with only primitive expressions (fully-expand and then evaluate)
+
+; -- Exercise 1.5
+
+; `p` is recursively calling itself.  If it's applicative-order evaluation, then it's going to attempt to evaluate the (p) combination before subsituting as parameters in the definition of the `test` procedure.
+; If it's normal-order evaluation, it's going to subsitute the operand expressions as-is into the `test` procedure definition.  Since the predicate is only going to evaluate the consequent (the expression that results if the predicate is true)
+
+; -- Exercise 1.6
+
+; The `if` special form only evaluates the alternate expression if the predicate is false.  Alyssa's custom `new-if` does not short-circuit during applicative-order evaluation and would evaluate all operands and would result in an infinite loop.
 
